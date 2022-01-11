df956785d06f6a0dfee9cb0e0d715569afcd41c3
oznurcet <71790893+oznurcet@users.noreply.github.com>
2022-01-11 22:47:59 +0300

    Add files via upload

---
+++ ödev7.sql
@@ -0,0 +1,15 @@
+SELECT rating, title FROM film
+GROUP BY rating;
+
+SELECT replacement_cost , COUNT(*) FROM film
+GROUP BY replacement_cost
+HAVING COUNT(*) > 50;
+
+SELECT store_id, COUNT(customer_id) FROM customer
+GROUP BY store_id;
+
+SELECT country_id , COUNT(city) FROM city
+GROUP BY country_id
+ORDER BY COUNT(city) DESC
+LIMIT 1;
+
