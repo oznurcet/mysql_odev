408fd3e9667394cca0816d3628cdd05fa40111b6
oznurcet <71790893+oznurcet@users.noreply.github.com>
2022-01-11 20:59:39 +0300

    Add files via upload

---
+++ ödev5.sql
@@ -0,0 +1,17 @@
+SELECT * FROM film 
+WHERE title LIKE '%n'
+ORDER BY length desc
+LIMIT 5;
+
+SELECT * FROM film
+WHERE title LIKE '%N'
+ORDER BY length 
+OFFSET 5
+LIMIT 5;
+
+SELECT * FROM customer 
+WHERE store_id = 1
+ORDER BY last_name DESC
+LIMIT 4
+
+
